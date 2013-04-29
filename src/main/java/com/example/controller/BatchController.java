8d707e89fff851a5a76c2165fe34ca777a4d7b28
Thys Michels <thysmichels@gmail.com>
2013-04-29 15:10:46 -0700

    Rename BulkController to BatchController

--- src/main/java/com/example/controller/BatchController.java
+++ src/main/java/com/example/controller/BatchController.java
@@ -10,29 +10,29 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.service.LoginService;
 
 @Controller
-@RequestMapping("/bulk")
-public class BulkController {
+@RequestMapping("/batch")
+public class BatchController {
 	
 	@Autowired
 	LoginService loginService;
 	
-	@RequestMapping(value="/new")
+	@RequestMapping(value="")
 	public String createBulkJobView(Map<String, Object> map)
 	{
 		map.put("sobjects", loginService.LoginToSalesforce().describeGlobal().getSObjects());
-		return "newbulkjob";
+		return "newbatchjob";
 	}
 	
 	public String queryforSelectedsObject(Map<String, Object> map)
 	{
 		//map.put("sObjectquery", value);
-		return "newbulkJob";
+		return "querybatchjob";
 	}
 	
 	@RequestMapping(value="/run", method=RequestMethod.POST)
 	public String runBulkJob(Map<String, Object> map)
 	{
-		return "runbulkjob";
+		return "runbatchjob";
 	}
 
 }
